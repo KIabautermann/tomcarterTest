b245c7c7f78ec41d1cfa43a0824b5e488891019a
Gaxxx420 <62268176+Gaxxx420@users.noreply.github.com>
2021-01-29 04:56:29 -0300

    Agregados y testeados los plugins necesarios para iniciar el proyecto
    Cambios detallados en Change Log.txt

---
+++ Unity Project/tomcarter project/Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.position += movement * 4 * Time.deltaTime;
+    }
+}
