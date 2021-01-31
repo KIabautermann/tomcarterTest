efec197d4844db577193a60d50f5b4e909be8f62
Gaxxx420 <62268176+Gaxxx420@users.noreply.github.com>
2021-01-30 22:51:27 -0300

    Added New Input System

---
+++ Unity Project/tomcarter project/Assets/Scripts/Player/PlayerBrain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBrain : MonoBehaviour
+{
+    private InputManager _myInputs;
+    private Rigidbody _rb;
+    private PlayerMovement _myMovement;
+
+    private void Start(){
+        _myInputs = GetComponent<InputManager>();
+        _rb = GetComponent<Rigidbody>();
+        _myMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update(){
+        if (_myInputs.jumpPerformed){
+            _myMovement.Jump();
+            _myInputs.jumpPerformed = false;
+        }
+        if(_myInputs.axis().x != 0)
+        {
+            _myMovement.Movement(_myInputs.axis().x);
+        }
+    }
+}
---
+++ Unity Project/tomcarter project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public void Movement(float direction)
+    {
+        Debug.Log("wants to move");
+    }
+    public void Jump()
+    {
+        Debug.Log("wants to jump");
+    }
+}
