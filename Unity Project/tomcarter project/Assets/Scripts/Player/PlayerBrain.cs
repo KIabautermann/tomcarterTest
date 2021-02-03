238e4145b88ee5dbd6d1eb9aea2bebda1541e69d
Gaxxx420 <62268176+Gaxxx420@users.noreply.github.com>
2021-02-03 19:49:33 -0300

    Music Added

--- Unity Project/tomcarter project/Assets/Scripts/Player/PlayerBrain.cs
+++ Unity Project/tomcarter project/Assets/Scripts/Player/PlayerBrain.cs
@@ -7,6 +7,8 @@ public class PlayerBrain : MonoBehaviour
     [SerializeField]
     private Transform[] _groundCheck;
     [SerializeField]
+    private Transform[] _wallCheck;
+    [SerializeField]
     private float _floorDetection;
     [SerializeField]
     private LayerMask _walkable;
@@ -21,14 +23,12 @@ public class PlayerBrain : MonoBehaviour
         facingRight = (transform.position.x + transform.right.x) > transform.position.x;
         wasFacingRight = facingRight;
     }
-
     private void Start(){
         _myInputs = GetComponent<InputManager>();
         _rb = GetComponent<Rigidbody>();
         _myMovement = GetComponent<Movement>();
         _myVisuals = GetComponent<CharacterVisuals>();
     }
-
     private void Update(){
         if (_myInputs.jumpPerformed){
             if (Grounded()){
@@ -45,6 +45,10 @@ public class PlayerBrain : MonoBehaviour
                 _myMovement.SetAcceleration(0);
                 wasFacingRight = facingRight;
             }
+        }
+        if(OnWall() && _myInputs.axis().x != 0)
+        {
+            _myMovement.SetAcceleration(0);
         }
     }
     private void FixedUpdate(){
@@ -63,9 +67,25 @@ public class PlayerBrain : MonoBehaviour
         }
         return counter != 0;
     }
+    bool OnWall()
+    {
+        int counter = 0;
+        for (int i = 0; i < _wallCheck.Length; i++)
+        {
+            if (Physics.Raycast(_wallCheck[i].position, transform.right, _floorDetection, _walkable))
+            {
+                counter++;
+            }
+        }
+        return counter != 0;
+    }
     private void OnDrawGizmos(){
         for (int i = 0; i < _groundCheck.Length; i++){
             Gizmos.DrawLine(_groundCheck[i].position, _groundCheck[i].position + -Vector3.up * _floorDetection);
+        }
+        for (int i = 0; i < _groundCheck.Length; i++)
+        {
+            Gizmos.DrawLine(_wallCheck[i].position, _wallCheck[i].position + transform.right * _floorDetection);
         }
     }
 }
