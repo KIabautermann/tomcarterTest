0bea4d601be2b728eb3725f55caf617b126de4c0
Pedro Mattenet Riva <pmattenet@gmail.com>
2021-04-18 20:47:53 -0300

    Revert "Tmp broken commit"
    This reverts commit b3743ad77ca67b950aaf1f6cae2ab63abdd0a56c.

--- tomcarter project/Assets/Scripts/StateMachine/PlayerStateMachine.cs
+++ tomcarter project/Assets/Scripts/StateMachine/PlayerStateMachine.cs
@@ -5,46 +5,66 @@ using TMPro;
 using System.Linq;
 using System;
 
-public class PlayerStateMachine : StateMachine
+public class PlayerStateMachine : MonoBehaviour
 {
     private State<PlayerStateMachine> _currentState;
     [SerializeField]
     private TextMeshProUGUI _currentStateDisplay;
     public PlayerData stats;
     private ComponentCache<State<PlayerStateMachine>> allStates;
-    
-    // void Start()
-    // {
-    //     var allComponents = GetComponents<State<PlayerStateMachine>>();
+    void Start()
+    {
+        var allComponents = GetComponents<State<PlayerStateMachine>>();
 
-    //     allStates = new ComponentCache<State<PlayerStateMachine>>(allComponents);
+        allStates = new ComponentCache<State<PlayerStateMachine>>(allComponents);
         
-    //     foreach(var state in allStates.GetAllInstances())
-    //     {
-    //         state.Init(this);
-    //     }
-    //     ChangeState<PlayerIdleState>();
-    // }
-    // public void ChangeState<T>() where T : State<PlayerStateMachine>
-    // {
-    //     if (!allStates.GetInstance<T>(out State<PlayerStateMachine> newState)) newState = null;
+        foreach(var state in allStates.GetAllInstances())
+        {
+            state.Init(this);
+        }
+        ChangeState<PlayerIdleState>();
+    }
+    public void ChangeState<T>() where T : State<PlayerStateMachine>
+    {
+        if (!allStates.GetInstance<T>(out State<PlayerStateMachine> newState)) newState = null;
               
-    //     if(_currentState != null)
-    //     {
-    //         if(newState == null || newState.OnCoolDown()) 
-    //         {
-    //             Debug.LogWarning($"Can't Transition to {typeof(T).ToString()}");
-    //             return;
-    //         }
+        if(_currentState != null)
+        {
+            if(newState == null || newState.OnCoolDown()) 
+            {
+                Debug.LogWarning($"Can't Transition to {typeof(T).ToString()}");
+                return;
+            }
 
-    //         if (_currentState.isExiting) return;
-    //         _currentState.TriggerTransitionOut();
-    //     }
+            if (_currentState.isExiting) return;
+            _currentState.TriggerTransitionOut();
+        }
 
-    //     //Debug.Log($"Transitioned to: {typeof(T).ToString()}");
-    //     _currentState = newState;
-    //     _currentStateDisplay.text = _currentState.stateName;
-    //     _currentState.TriggerTransitionIn();
-    // }
+        //Debug.Log($"Transitioned to: {typeof(T).ToString()}");
+        _currentState = newState;
+        _currentStateDisplay.text = _currentState.stateName;
+        _currentState.TriggerTransitionIn();
+    }
+    private void Update() 
+    {
+        if(_currentState != null)
+        {
+            _currentState.TriggerLogicUpdate();
+        }
+    }
+    private void FixedUpdate() 
+    {
+        if(_currentState != null)
+        {
+            _currentState.TriggerPhysicsUpdate();
+        }
+    }
+
+    public bool IsCurrentState(State<PlayerStateMachine> state) 
+    {
+        return state == _currentState;
+    }
+    
+
    
 }
