1bb46ce4fc6cdff9ad9f1547e4a1b43f04f3e305
Gaxxx420 <62268176+Gaxxx420@users.noreply.github.com>
2021-08-09 15:30:37 -0300

    Animations Scriptable Object

---
+++ tomcarter project/Assets/Scripts/Player/Data/PlayerEffectsData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPlayerData", menuName = "Data/AnimationData")]
+public class PlayerEffectsData : ScriptableObject
+{
+   public AnimationClip dashAfterimage;
+   public AnimationClip dashjumpUpAfterimage;
+   public AnimationClip dashjumpDownAfterimage;
+   public AnimationClip hardenAfterimage;
+   public AnimationClip dashjumpWind;
+   public AnimationClip sporeDashTrail;
+}
