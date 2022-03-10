51a7e30fec38321995142671115b29466b96034a
Pedro Mattenet Riva <pmattenet@gmail.com>
2022-03-10 02:01:26 -0300

    Clear cache

--- tomcarter project/Assets/Scripts/Player/Skills/AbilitySystem.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AbilitySystem 
-{
-   public abstract List<State<PlayerStateMachine>> GetAvailableStates();
-}
--- tomcarter project/Assets/Scripts/Player/Skills/UnlockedAbilitiesData.cs
+++
@@ -1,9 +0,0 @@
-﻿
-[System.Serializable]
-public class UnlockedAbilitiesData {
-    public bool _hasSporeDashUnlocked = false;
-    public bool _hasRootAttackUnlocked = false;
-    public bool _hasVineHookUnlocked = false;
-    public bool _hasBarkGuardUnlocked = false;
-    public bool _hasHedgeClimbUnlocked = false;
-}
